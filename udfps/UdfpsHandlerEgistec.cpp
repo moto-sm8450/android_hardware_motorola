7a8c1864f411f35d1dfc74018ceb63002074907f
Nick Reuter <nreuter85@gmail.com>
2026-02-13 18:49:14 -0600

    rebase into HEAD^ when tested
    Change-Id: I155a277220dfbe4d0e63f52aa8d581942e162c26

--- udfps/UdfpsHandlerEgistec.cpp
+++ udfps/UdfpsHandlerEgistec.cpp
@@ -25,9 +25,9 @@ class EgistecUdfpsHandler : public UdfpsHandler {
     }
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
-#ifdef ENABLE_SCREENOFF_SLEEP
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-#endif
+        if (SCREEN_WAKE_DELAY > 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(SCREEN_WAKE_DELAY));
+        }
         enableHighBrightFod();
         std::thread([this]() {
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
